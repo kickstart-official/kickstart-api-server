ed99c4307f8cbf63affb4daa9848f29c01790e5e
chitabathina-kickstart <chitabathina.kickstart@gmail.com>
2019-02-23 11:05:45 -0800

    first push with sample application

---
+++ src/test/java/com/api/kickstartService/KickstartServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.api.kickstartService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KickstartServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
